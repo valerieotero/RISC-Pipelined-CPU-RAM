68f7dc423bf5afa8592b9694f01fd7cc2535ed36
Valerie <valerie.otero2@upr.edu>
2020-09-28 12:19:41 -0400

    can read now from data memory

--- data_mem_tb.v
+++ data_mem_tb.v
@@ -3,8 +3,8 @@ module data_mem_tb;
 
 integer file, fw, code, i; reg [31:0] data;
 reg Enable, ReadWrite; reg[31:0]DataIn;
-reg [31:0] Address; wire [31:0] DataOut;
-data_ram256x8 ram1 (DataOut, Enable, ReadWrite, Address, DataIn);
+reg [31:0] Address; wire [31:0] DataOut; reg[1:0]Size;
+data_ram256x8 ram1 (DataOut, Enable, ReadWrite, Address, DataIn, Size);
 
 //Pre-charge memory
 initial begin
@@ -21,18 +21,45 @@ end
 
 initial begin
     fw = $fopen("data_memcontent.txt", "w");
-    Enable = 1'b0; ReadWrite = 1'b0;
     Address = #1 32'b00000000000000000000000000000000; //make sure adress is in 0 after precharge
-    repeat (18) begin
-    #5 Enable = 1'b1;
-    #5 Enable = 1'b0;
-    Address = Address + 1;
-end
+    Enable = 1'b0; ReadWrite = 1'b0; //Read  
+
+
+    $fdisplay(fw, "-------------- Reading Word from Addresses 0, 4, 8 and 12 ----------------\n");   
+    Size = 2'b10; //WORD
+    ReadWrite = 1'b0; //Read
+    repeat (4) begin          
+        #5 Enable = 1'b1;
+        #5 Enable = 1'b0;
+        #1 $fdisplay(fw,"ReadWrite: %d -- Address: %d -- DataIn: %b -- DataOut: %b -- Time: %d",ReadWrite, Address, DataIn, DataOut, $time);
+        Address = Address + 4;
+    end 
+
+
+    $fdisplay(fw, "\n\n-------------- Reading Byte from Address 0 ----------------\n");   
+    Size = 2'b00; //BYTE
+    ReadWrite = 1'b0; //Read
+    Address = 0;
+    repeat (1) begin          
+        #5 Enable = 1'b1;
+        #5 Enable = 1'b0;
+        #1 $fdisplay(fw,"ReadWrite: %d -- Address: %d -- DataIn: %b -- DataOut: %b -- Time: %d",ReadWrite, Address, DataIn, DataOut, $time);
+       // Address = Address + 1;
+    end  
+
+       
+    $fdisplay(fw, "\n\n-------------- Reading Half-Word from Addresses 2 and 4 ----------------\n");   
+    Size = 2'b01; //HALF-WORD
+    ReadWrite = 1'b0; //Read
+    Address = 2;
+    repeat (2) begin          
+        #5 Enable = 1'b1;
+        #5 Enable = 1'b0;
+        #1 $fdisplay(fw,"ReadWrite: %d -- Address: %d -- DataIn: %b -- DataOut: %b -- Time: %d",ReadWrite, Address, DataIn, DataOut, $time);
+        Address = Address + 2;
+    end   
+
+
 $finish;
-end
-always @ (posedge Enable)
-    begin
-    #1;   
-    $fdisplay(fw,"ReadWrite: %d -- Address: %d -- DataIn: %b -- DataOut: %b-- Time: %d",ReadWrite, Address, DataIn, DataOut, $time);
 end
 endmodule
--- mem.v
+++ mem.v
@@ -20,20 +20,52 @@ endmodule
               
 
 //DATA MEMORY
-module data_ram256x8(output reg[31:0] DataOut, input Enable, ReadWrite, input[31:0] Address, input[31:0] DataIn);
+module data_ram256x8(output reg[31:0] DataOut, input Enable, ReadWrite, input[31:0] Address, input[31:0] DataIn, input [1:0] Size);
 
     reg[7:0] Mem[0:255]; //256 localizaciones 
 
     always @ (Enable, ReadWrite)
-        if (Enable) //When Enable = 1
+        if (Enable) //When Enable = 1        
         begin
-            if (ReadWrite) //When ReadWrite = 1
-            begin
-                Mem[Address] = DataIn; 
+
+            casez(Size) //"casez" to ignore dont care values
+            2'b00: //BYTE
+            begin 
+                if (ReadWrite) //When Write 
+                begin
+                    Mem[Address] = DataIn; 
+                end
+                else //When Read
+                begin
+                    DataOut= Mem[Address];
+                end                
             end
-            else //When ReadWrite = 0
+
+              2'b01: //HALF-WORD
             begin
-                DataOut= Mem[Address];
+                if (ReadWrite) //When Write 
+                begin
+                    Mem[Address] = DataIn; 
+                end
+                else //When Read
+                begin
+                     DataOut = {Mem[Address+0], Mem[Address+1]}; 
+                end  
             end
+
+            2'b10: //WORD
+            begin
+                if (ReadWrite) //When Write 
+                begin
+                    Mem[Address] = DataIn; 
+                end
+                else //When Read
+                begin
+                     DataOut = {Mem[Address+0], Mem[Address+1], Mem[Address+2], Mem[Address+3]}; 
+                end  
+            end
+          
+            endcase
+            
         end
 endmodule
