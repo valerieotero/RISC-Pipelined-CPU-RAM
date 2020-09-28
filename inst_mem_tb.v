28f8d96d24b8c92e45c90574426a0cd4e3fa0391
Valerie <valerie.otero2@upr.edu>
2020-09-27 21:00:45 -0400

    done with instruction ram

--- inst_mem_tb.v
+++ inst_mem_tb.v
@@ -4,9 +4,11 @@ module instmem_tb;
 integer file, fw, code, i; reg [31:0] data;
 reg Enable;
 reg [31:0] Address; wire [31:0] DataOut;
+
 inst_ram256x8 ram1 (DataOut, Enable, Address );
 
-initial begin
+initial
+    begin
     file = $fopen("inst_input_file.txt","rb");
     Address = 32'b00000000000000000000000000000000;
         while (!$feof(file)) begin //while not the end of file
@@ -15,14 +17,14 @@ initial begin
         Address = Address + 1;
     end
 
-$fclose(file);
+$fclose(file);  
 end
 
 initial begin
     fw = $fopen("inst_memcontent.txt", "w");
     Enable = 1'b0; 
     Address = #1 32'b00000000000000000000000000000000; //make sure adress is in 0 after precharge
-    repeat (17) begin
+    repeat (16) begin
     #5 Enable = 1'b1;
     #5 Enable = 1'b0;
     Address = Address + 1;
@@ -32,6 +34,6 @@ end
 always @ (posedge Enable)
     begin
     #1;   
-    $fdisplay(fw,"data en %d = %b %d", Address, DataOut, $time);
+    $fdisplay(fw,"Data en %d = %h %d", Address, DataOut, $time);
 end
 endmodule
--- mem.v
+++ mem.v
@@ -4,24 +4,22 @@
 //R/W– Read/Write operation (0- read, 1 write)
 
 //INSTRUCTION MEMORY 
-
 module inst_ram256x8(output reg[31:0] DataOut, input Enable, input [31:0]Address);
                   
-   reg[7:0] Mem[0:255]; //256 localizaciones de 32-bits
+   reg[7:0] Mem[0:255]; //256 localizaciones 
    
     always @ (Enable)
         if (Enable) //When Enable = 1            
             if(Address==0 || Address%4==0) 
             begin    
-                DataOut = {Mem[Address], Mem[Address+1], Mem[Address+2], Mem[Address+3]};                
+                DataOut = {Mem[Address+0], Mem[Address+1], Mem[Address+2], Mem[Address+3]};                
             end
             else
-                DataOut= Mem[Address];                                                 
-                
-endmodule 
+                DataOut= Mem[Address];   
+endmodule                              
+              
 
-
-//DATA
+//DATA MEMORY
 module data_ram256x8(output reg[31:0] DataOut, input Enable, ReadWrite, input[31:0] Address, input[31:0] DataIn);
 
     reg[31:0] Mem[0:255]; //256 localizaciones de 32-bits
